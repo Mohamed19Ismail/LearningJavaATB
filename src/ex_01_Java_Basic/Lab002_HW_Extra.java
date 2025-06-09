064cee5c8b923c5208604a016ff24babf618f433
Mohamed19Ismail <ismail19mba@gmail.com>
2025-06-09 19:43:01 +0530

    2 pushed

--- src/ex_01_Java_Basic/Lab002_HW_Extra.java
+++ src/ex_01_Java_Basic/Lab002_HW_Extra.java
@@ -3,6 +3,7 @@ package ex_01_Java_Basic;
 public class Lab002_HW_Extra {
     public static void main(String[] args) {
         System.out.println("Add two numbers" + "2+2");
+        System.out.println("I have pushed it");
 
     }
 }
