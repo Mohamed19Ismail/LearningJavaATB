23a282cdff31f18439c0933060a551bde3e8d29f
Mohamed19Ismail <ismail19mba@gmail.com>
2025-07-21 14:51:00 +0530

    functions

--- src/ex_14_String/InterviewCoding_Question.java
+++ src/ex_14_String/InterviewCoding_Question.java
@@ -10,7 +10,16 @@ public class InterviewCoding_Question {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter a String");
         String input = scanner.next();
+        //String reversestr = reverseStringSB(input);
+        //System.out.println(reversestr);
 
+        String reversedstr = reverseStr(input);
+
+        if(reversedstr.equalsIgnoreCase(input)){
+            System.out.println("Palindrome");
+        }else {
+            System.out.println("Not Palindrome");
+        }
 
 //        StringBuffer str = new StringBuffer(scanner.next());
 //        String reverse = String.valueOf(str.reverse());
@@ -19,8 +28,15 @@ public class InterviewCoding_Question {
 
         //System.out.println(str);
     }
-    static String reverseString(String input){
-
-        return "1";
+    static String reverseStr(String input){
+        String reversedString = "";
+        for(int i=input.length()-1;i>=0;i--){
+            reversedString = reversedString + input.charAt(i);
+        }
+        return reversedString;
     }
+//    static String reverseStringSB(String input){
+//        StringBuilder sb = new StringBuilder(input);
+//        return sb.reverse().toString();
+//    }
 }
